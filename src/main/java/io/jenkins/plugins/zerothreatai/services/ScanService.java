641156445d87c704c8a1f9224f890cea87c082c3
JeelGajera <jeelgajera200@gmail.com>
2025-04-28 16:02:13 +0530

    chore: refactor zt-plugin for plugin hosting request suggested changes

---
+++ src/main/java/io/jenkins/plugins/zerothreatai/services/ScanService.java
@@ -0,0 +1,71 @@
+package io.jenkins.plugins.zerothreatai.services;
+
+import net.sf.json.JSONObject;
+
+import java.io.PrintStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.Date;
+import java.util.Scanner;
+import java.util.concurrent.TimeUnit;
+
+import io.jenkins.plugins.zerothreatai.models.ScanResponse;
+
+public class ScanService {
+    private static final String SCAN_API_URL = "https://api.zerothreat.ai/api/scan/devops";
+    private static final String ZT_TOKEN_HEADER_KEY = "zt-token";
+
+    public static ScanResponse initiateScan(String token) {
+        try {
+            HttpURLConnection conn = (HttpURLConnection) new URL(SCAN_API_URL).openConnection();
+            conn.setRequestMethod("POST");
+            conn.setRequestProperty("Content-Type", "application/json");
+            conn.setRequestProperty(ZT_TOKEN_HEADER_KEY, token);
+            conn.setDoOutput(true);
+
+            try (Scanner scanner = new Scanner(conn.getInputStream()).useDelimiter("\\A")) {
+                String response = scanner.hasNext() ? scanner.next() : "";
+                scanner.close();
+
+                JSONObject jsonResponse = JSONObject.fromObject(response);
+                ScanResponse scanResponse = new ScanResponse();
+                scanResponse.Status = jsonResponse.getInt("status");
+                scanResponse.Message = jsonResponse.getString("message");
+                scanResponse.Code = jsonResponse.getString("code");
+                scanResponse.ScanStatus = jsonResponse.getInt("scanStatus");
+                scanResponse.Url = jsonResponse.getString("url");
+                return scanResponse;
+            }
+        } catch (Exception e) {
+            String message = e.getMessage();
+            return new ScanResponse(message);
+        }
+    }
+
+    public static boolean pollScanStatus(String token, String code, PrintStream logger) {
+        int status = 1;
+        while (status < 4) {
+            try {
+                TimeUnit.SECONDS.sleep(300);
+                HttpURLConnection conn = (HttpURLConnection) new URL(SCAN_API_URL + "/" + code).openConnection();
+                conn.setRequestMethod("GET");
+                conn.setRequestProperty(ZT_TOKEN_HEADER_KEY, token);
+
+                Scanner scanner = new Scanner(conn.getInputStream()).useDelimiter("\\A");
+                String response = scanner.hasNext() ? scanner.next() : "";
+                scanner.close();
+
+                JSONObject jsonResponse = JSONObject.fromObject(response);
+                status = jsonResponse.getInt("scanStatus");
+
+                logger.println("Scan is inprogress... [ " + new Date() + " ]");
+
+            } catch (Exception e) {
+                logger.println("Error polling scan status: " + e.getMessage());
+                return false;
+            }
+        }
+        return true;
+    }
+
+}
